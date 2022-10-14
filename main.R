5b153be776cd32096eaf1afcbec671773cfbc1aa
agouy <contact@agouy.com>
2022-10-14 07:43:15 +0000

    first commit

--- main.R
+++ main.R
@@ -1,11 +1,45 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(survival)
+library(broom)
+library(survminer)
+library(tim)
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+df <- ctx$select(c(".y", ".x", ".ci", ".ri", ctx$colors))
+
+cov_names <- unlist(ctx$colors)
+form_right <- paste0(cov_names, collapse = " + ")
+form <- formula(paste0("Surv(.x, .y) ~ ", form_right))
+
+fit <- surv_fit(form, data = df)
+
+p <- ggsurvplot(
+  fit,
+  data = df,
+  size = 0.5,
+  conf.int = TRUE,
+  pval = FALSE,
+  risk.table = FALSE,
+  font.title = 10,
+  font.subtitle = 8,
+  font.caption = 8,
+  font.legend = 8,
+  font.x = 8,
+  font.y = 8,
+  font.tickslab = 8,
+  ggtheme = theme_bw()
+)
+
+plot_file <- tim::save_plot(p$plot, width = 3, height = 3, units = "in")
+
+df_plot <- tim::plot_file_to_df(plot_file) %>%
+  mutate(.ci = 0L, .ri = 0L) %>%
+  ctx$addNamespace()
+
+df_out <- tidy(fit) %>%
+  mutate(.ci = 0L, .ri = 0L) %>%
+  ctx$addNamespace()
+
+ctx$save(list(df_out, df_plot))
